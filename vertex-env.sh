a92649b66af59dec68cb30ecd87bf7f1c4a78ae5
Maciej Jaśkowski <maciej.jaskowski@gmail.com>
2014-01-28 23:17:50 +0100

    working environment

---
+++ vertex-env.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get -y install unzip
+sudo apt-get -y install vim
+sudo apt-get -y install python-software-properties python g++ make
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get -y install nodejs=0.10.25-1chl1~precise1
+
+sudo apt-get -y install openjdk-7-jdk
+
+wget -c http://dl.bintray.com/vertx/downloads/vert.x-2.1M3.zip vert.x.zip
+unzip vert.x-2.1M3.zip  
+echo "PATH=\"$PATH:/home/vagrant/vert.x-2.1M3/bin/\"" >> ~/.bashrc
